b4e8ce5bc31d690a15bbd6027de45b462cfffc1a
Nhan2108cute <145674566+Nhan2108cute@users.noreply.github.com>
2025-06-16 13:45:28 +0700

    cap nhat article

--- src/main/java/com/example/backend/model/User.java
+++ src/main/java/com/example/backend/model/User.java
@@ -18,7 +18,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-    private Long id;
+    private int id;
     private int accountType ;
     private String username;
     private String password;
@@ -28,58 +28,5 @@ public class User {
     @Column(name = "birthday")
     private LocalDate birthday;
 public User(){}
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public LocalDate getBirthday() {
-        return birthday;
-    }
-
-    public int getAccountType() {
-        return accountType;
-    }
-
-    public void setAccountType(int accountType) {
-        this.accountType = accountType;
-    }
-    public void setBirthday(LocalDate birthday) {
-        this.birthday = birthday;
-    }
 }
